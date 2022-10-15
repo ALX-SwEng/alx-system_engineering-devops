5b46e8f9d43bd0ee78ac88cb3fbd32bc24b0f0b5
Chere Lemma <chere.lemma@aastu.edu.et>
2022-10-15 13:20:11 +0300

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install flask from pip3
 
-papython::pip { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
